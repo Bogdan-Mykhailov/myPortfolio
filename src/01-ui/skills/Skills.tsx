294e75f09016c2ea0b9108f0e701ea41287abb4e
Bogdan <bogdan_mykhailov@icloud.com>
2022-12-18 15:48:10 +0200

    :construction: add skill component, edit props for skill

--- src/01-ui/skills/Skills.tsx
+++ src/01-ui/skills/Skills.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './Skills.module.css'
 import styles from '../../common/styles/Container.module.css'
 import {Skill} from "./skill/Skill";
+import tel from '../../assets/img/phone-solid.svg'
 
 export const Skills = () => {
   return (
@@ -9,9 +10,24 @@ export const Skills = () => {
       <div className={`${styles.container} ${s.skillsContainer}`}>
         <h2 className={s.title}>Skills</h2>
         <div className={s.skillsWrapper}>
-          <Skill />
-          <Skill />
-          <Skill />
+          <Skill
+            title='React'
+            icon={tel}
+            imgDescription='Skill icon'
+            skillDescription='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsum laborum laudantium modi quo voluptates.'
+          />
+          <Skill
+            title='React'
+            icon={tel}
+            imgDescription='Skill icon'
+            skillDescription='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsum laborum laudantium modi quo voluptates.'
+          />
+          <Skill
+            title='React'
+            icon={tel}
+            imgDescription='Skill icon'
+            skillDescription='Lorem ipsum dolor sit amet, consectetur adipisicing elit. Error ipsum laborum laudantium modi quo voluptates.'
+          />
         </div>
       </div>
     </section>
--- src/01-ui/skills/skill/Skill.tsx
+++ src/01-ui/skills/skill/Skill.tsx
@@ -1,11 +1,23 @@
 import React from 'react'
 import s from './Skill.module.css';
 
+interface ISkill {
+  title: string
+  icon: string
+  imgDescription: string
+  skillDescription: string
+}
 
-export const Skill = () => {
+export const Skill = ({
+                        title,
+                        icon,
+                        imgDescription,
+                        skillDescription }: ISkill) => {
   return (
     <div className={s.skill}>
-      Skill
+      <img className={s.icon} src={icon} alt={imgDescription}/>
+      <h3 className={s.title}>{title}</h3>
+      <p className={s.description}>{skillDescription}</p>
     </div>
   );
 };
