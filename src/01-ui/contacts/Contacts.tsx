8ae2051947fb45bfd7afe3d146fd8a0cbd749343
bogdan_mykhailov <bogdan_mykhailov@icloud.com>
2022-12-19 12:41:54 +0200

    :construction: continue contacts component

---
+++ src/01-ui/contacts/Contacts.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import s from './Contacts.module.css'
+
+export const Contacts = () => {
+  return (
+    <div className={s.contacts}>
+
+    </div>
+  );
+};
--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import {Header} from "./01-ui/header/Header";
 import {Main} from "./01-ui/main/Main";
 import {Skills} from "./01-ui/skills/Skills";
 import {Portfolio} from "./01-ui/portfolio/Portfolio";
+import {Contacts} from "./01-ui/contacts/Contacts";
 
 export const App = () => (
   <div className={s.app}>
@@ -11,5 +12,6 @@ export const App = () => (
     <Main/>
     <Skills/>
     <Portfolio/>
+    <Contacts/>
   </div>
 );
