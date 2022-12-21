07e1d078224f7132608f5413e31f47467ee2056f
bogdan_mykhailov <bogdan_mykhailov@icloud.com>
2022-12-21 11:22:53 +0200

    :lipstick: edit styles for, portfolio, skills, about, header, footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import s from './App.module.css';
-import {Header} from "./01-ui/header/Header";
-import {Main} from "./01-ui/main/Main";
-import {Skills} from "./01-ui/skills/Skills";
-import {Portfolio} from "./01-ui/portfolio/Portfolio";
-import {Contacts} from "./01-ui/contacts/Contacts";
-import {Footer} from "./01-ui/footer/Footer";
-import {About} from "./01-ui/about/About";
+import {Header} from "./ui/header/Header";
+import {Main} from "./ui/main/Main";
+import {Skills} from "./ui/skills/Skills";
+import {Portfolio} from "./ui/portfolio/Portfolio";
+import {Contacts} from "./ui/contacts/Contacts";
+import {Footer} from "./ui/footer/Footer";
+import {About} from "./ui/about/About";
 
 export const App = () => (
   <div className={s.app}>
--- src/ui/contacts/Contacts.tsx
+++ src/ui/contacts/Contacts.tsx
@@ -7,14 +7,12 @@ export const Contacts = () => {
     <section id='contacts' className={s.contacts}>
       <div className={`${styles.container} ${s.contactsContainer}`}>
         <h2 className={s.title}>Contacts</h2>
-        <div className={s.contactsWrapper}>
-          <form className={s.contactsInfo} method='post' action="http://localhost:3000">
+          <form className={s.contactsForm} method='post' action="http://localhost:3000">
             <input className={s.input} type="text"/>
             <input className={s.input} type="email"/>
             <textarea className={s.textarea} name="" id="" cols={30} rows={10}></textarea>
-          </form>
             <button className={s.button} type="submit">Submit</button>
-        </div>
+          </form>
       </div>
     </section>
   );
